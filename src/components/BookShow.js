165a183292ebb8604f005e9a80d2aec700a8decc
RyanRoman <C00412434@louisiana.edu>
2023-07-18 20:21:30 -0500

    add, read, edit objects, include photos

---
+++ src/components/BookShow.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete, onEdit }) {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  // communicate this down as prop 'onSubmit' to bookEdit component
+  // do multiple things when submitted, get back id and new title
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
+  }
+
+  return (
+    <div className="book-show">
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;
